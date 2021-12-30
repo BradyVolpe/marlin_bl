65e98139837a10bbb7a14bd32a15ec6e583ac43c
Brady Volpe <brady.volpe@nimble-this.com>
2021-12-30 18:53:03 -0500

    enabled z offset wizard

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -813,7 +813,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.00, 80.00, 400.00, 416 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.00, 80.00, 400.00, 416.6 }
 
 /**
  * Default Max Feed Rate (mm/s)
